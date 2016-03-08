78307f0416a7dd6fb58c3dc5c8dd534758402918
dilliraja-coax <dilliraja.coax@gmail.com>
2016-03-08 10:55:38 +0530

    static pages updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Coax Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
